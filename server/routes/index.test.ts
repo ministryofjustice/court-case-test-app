03c4e4b7e9206454448d0ce8b7fc8158d7325e8d
Sarahyjja <sarah.kharraz@digital.justice.gov.uk>
2021-10-29 14:12:04 +0100

    Retrieving Date of Hearing on crimePortal page

--- server/routes/index.test.ts
+++ server/routes/index.test.ts
@@ -23,13 +23,15 @@ describe('GET /', () => {
   })
 })
 
-// describe('POST /', () => {
-//   it('should render the date of hearing', () => {
-//     return request(app)
-//       .post('/')
-//       .expect('Content-Type', /html/)
-//       .expect(res => {
-//         expect(res.text).toContain('This site is under construction...')
-//       })
-//   })
-// })
+// TODO: Test with Wiremock back-end
+describe('POST /', () => {
+  it('should render the date of hearing', () => {
+    const todayDate = new Intl.DateTimeFormat('en-GB').format(new Date())
+    return request(app)
+      .post('/crimePortal')
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain(todayDate)
+      })
+  })
+})
