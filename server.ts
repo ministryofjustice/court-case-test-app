cee785628f556835912a49f0aed0cb2131fc91aa
sarahyjja <44409053+sarahyjja@users.noreply.github.com>
2021-11-01 15:38:24 +0100

    Add banner (#15)
    * Add banner
    
    * Correcting issue with banner
    
    * EsLint issue on console the banner

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
+import fs from 'fs'
+import path from 'path'
 import { initialiseAppInsights, buildAppInsightsClient } from './server/utils/azureAppInsights'
 
 initialiseAppInsights()
@@ -12,5 +14,8 @@ import app from './server/index'
 import logger from './logger'
 
 app.listen(app.get('port'), () => {
+  const data = fs.readFileSync(path.join(__dirname, '../assets/banner.txt'), 'utf8')
+  // eslint-disable-next-line no-console
+  console.log(data.toString())
   logger.info(`Server listening on port ${app.get('port')}`)
 })
